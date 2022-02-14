6713c84aae15ebe2ecbe6a5e0f19c14797b2ee33
Sreehari S <sreehari.pdsy@gmail.com>
2022-02-14 10:08:10 +0530

    final

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,66 @@
 
 
+
+//MOBILE-NAVIGATION
+(function() {
+  "use strict"; 
+
+  //Easy selector helper function  
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+  //Easy event listener function
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+  //Easy on scroll event listener 
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }  
+  //Navbar links active state on scroll
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive) 
+  //Mobile nav toggle
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  }) 
+})()
+
+
 //section-three
 function picture() { 
   var pic = "assets/images/soc main.svg"
   document.getElementById('img-section-3').src = pic.replace('90x90', '225x225');
   document.getElementById('img-section-3').style.display='block';
 }
-
-  
 //section-seven
 document.getElementById("target-me").open = true;
 const details = document.querySelectorAll("details");
@@ -20,4 +73,3 @@ details.forEach((targetDetail) => {
     });
   });
 });
-
